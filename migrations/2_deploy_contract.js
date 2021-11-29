3d0c92c3155297b2e61b1e70655e7759e511c6ff
Roman <romanfoltyn1@gmail.com>
2021-11-29 18:29:06 +0200

    create token

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 const TokenSale = artifacts.require("TokenSale");
+const Token = artifacts.require("Token");
 
 module.exports = function (deployer) {
    deployer.deploy(TokenSale, "Hello world!");
+   deployer.deploy(Token);
 };
