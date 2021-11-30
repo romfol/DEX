efdaf4128c14db43f929b1ce9f844531213afa96
Roman <romanfoltyn1@gmail.com>
2021-11-30 13:21:28 +0200

    resolve decimals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor() ERC20("Folton", "FTN") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _mint(msg.sender, 1000000 * 10 ** 8);
     }
 }
